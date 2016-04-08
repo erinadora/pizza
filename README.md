b6a68aea9123f099ea04672e4deb8ea487c7a299
eg <eringoncer@gmail.com>
2016-04-08 15:09:33 -0700

    add functionality and styling

---
+++ README.md
@@ -0,0 +1,28 @@
+# A Pizza My Heart
+
+#### A Pizza Ordering Program for Epicodus, 4.8.2016
+
+#### By **Erin Goncer**
+
+## Description
+
+This program allows the user to create a pizza and have the pizza generate a price based on the selections.
+
+## Setup/Installation Requirements
+
+* Clone this repository.
+* Open file.
+
+## Support and Contact Details
+
+For any questions, comments, or inquiries, please contact Erin Goncer.
+
+## Technologies Used
+
+HTML, CSS, Bootstrap, jQuery, JS
+
+### License
+
+*This software is licensed under the MIT license.*
+
+Copyright (c) 2016 **Erin Goncer**
--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 //BUSINESS LOGIQUE
-function Pizza(toppings, scale, price) {
+function Pizza(toppings, size, sauce, base) {
   this.toppings = toppings;
-  this.scale = scale;
-  this.price = price
+  this.size = size;
+  this.sauce = sauce;
+  this.base = base;
 }
 
-var smallPizza = new Pizza()
-
-function check() {
-    document.getElementById("myCheck").checked = true;
+Pizza.prototype.orderTotal = function() {
+  return total = parseInt(this.toppings)+ parseInt(this.size) + parseInt(this.sauce) + parseInt(this.base);
 }
-
-function uncheck() {
-    document.getElementById("myCheck").checked = false;
-}
-
 
 
 //UI LOGIQUE
 $(document).ready(function() {
-  $("#piz").fadeIn(1000).fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000);
+  //Some... fun styling stuff:
+  $("#piz").fadeIn(1000).fadeOut(1000).fadeIn(1000).addClass("red").fadeOut(1000).fadeIn(1000);
 
   $("img#hart").fadeIn(1000).fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(500).fadeOut(100).fadeIn(200).fadeOut(200).fadeIn(200);
 
@@ -27,5 +22,25 @@ $(document).ready(function() {
     $(this).toggle(500);
   });
 
+  $("form#newOrder").submit(function(event) {
 
+  event.preventDefault();
+
+  var pizzaSize = parseInt($(".size").val());
+  var pizzaTopping = parseInt($(".top").val());
+  var pizzaSauce = parseInt($(".sauce").val());
+  var pizzaBase = parseInt($(".base").val());
+
+  var newPizza = new Pizza (pizzaTopping, pizzaSize, pizzaSauce, pizzaBase);
+
+  newPizza.orderTotal();
+
+  // if (pizzaTopping === "pineapple") {
+  //   $(".pizza").addClass("pineapples");
+  $(".pizza").addClass
+
+  alert("$" + total);
+
+
+  });
 });
